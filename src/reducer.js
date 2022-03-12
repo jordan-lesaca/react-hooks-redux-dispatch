a6fa043cc4e1494d461e839f485df62b4f3b21b6
Jordan Lesaca <Jordanlesaca@gmail.com>
2022-03-12 00:15:46 -0800

    done

--- src/reducer.js
+++ src/reducer.js
@@ -1,13 +1,20 @@
-function changeState(state, action) {
-  switch (action.type) {
-    case "counter/increment":
-      return { count: state.count + 1 };
-    default:
-      return state;
-  }
-}
-
 let state = { count: 0 };
-let action = { type: "counter/increment" };
-
-changeState(state, action);
+ 
+function changeState(state, action) {
+ switch (action.type) {
+   case "counter/increment":
+     return { count: state.count + 1 };
+   default:
+     return state;
+ }
+}
+ 
+function render() {
+ document.body.textContent = state.count;
+}
+ 
+function dispatch(action) {
+ state = changeState(state, action);
+ render();
+}
+render();
